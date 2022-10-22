08f7913db980724c5415b2fc731e884054ce3ddc
Harshil Shah <38115399+Harshil-Shah99@users.noreply.github.com>
2022-10-22 03:07:24 -0400

    Add files via upload

---
+++ evolution_experiments.py
@@ -0,0 +1,22 @@
+from nni.experiment import Experiment
+start = time.time()
+experiment = Experiment('local')
+search_space = {
+    'lr': {'_type': 'loguniform', '_value': [0.0001, 0.1]},
+    'momentum': {'_type': 'uniform', '_value': [0, 1]},
+}
+
+experiment.config.trial_command = 'python main.py --epochs 10'
+experiment.config.trial_code_directory = '.'
+experiment.config.search_space = search_space
+experiment.config.tuner.name = 'Evolution'
+experiment.config.tuner.class_args = {
+    'optimize_mode': 'maximize',
+    'population_size': 6,
+}
+experiment.config.max_trial_number = 10
+experiment.config.trial_concurrency = 2
+experiment.run(8080)
+print('Time taken for evolution experiments: {:.4f}'.format(end - start))
+input('Press enter to quit')
+experiment.stop()
---
+++ tpe_experiments.py
@@ -0,0 +1,24 @@
+from nni.experiment import Experiment
+start = time.time()
+experiment = Experiment('local')
+search_space = {
+    'lr': {'_type': 'loguniform', '_value': [0.0001, 0.1]},
+    'momentum': {'_type': 'uniform', '_value': [0, 1]},
+}
+
+experiment.config.trial_command = 'python main.py --epochs 10'
+experiment.config.trial_code_directory = '.'
+experiment.config.search_space = search_space
+experiment.config.tuner.name = 'TPE'
+experiment.config.tuner.class_args = {
+    'optimize_mode': 'maximize',
+    'tpe_args': {
+        'constant_liar_type': 'best',
+    }
+}
+experiment.config.max_trial_number = 10
+experiment.config.trial_concurrency = 2
+experiment.run(8080)
+print('Time taken for evolution experiments: {:.4f}'.format(end - start))
+input('Press enter to quit')
+experiment.stop()
